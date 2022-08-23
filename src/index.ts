edd04e79e8b8eaf20f7f82d84588dd223205f839
Cal <calum.moore@gmail.com>
2022-08-23 21:20:55 +0100

    Export addition modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './Spacetime'
 export * from './Collection'
 export * from './Client'
+export * from './Query'
+export * from './Doc'
+export * from './Subscription'
 export * from './types'
