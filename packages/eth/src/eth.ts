992fdf9fdb3e1bc78d25e7da2db4d4ae6aab8dbf
Cal <calum.moore@gmail.com>
2022-11-22 11:33:30 +0000

    Clean up exports

--- packages/eth/src/eth.ts
+++ packages/eth/src/eth.ts
@@ -2,6 +2,7 @@
 import {
   x25519xsalsa20poly1305,
   decodeFromString,
+  stringifyEncryptedData,
 } from '@polybase/util'
 
 export const win: any = (typeof self === 'object' && self.self === self && self) ||
@@ -26,7 +27,7 @@ export async function requestAccounts (): Promise<string[]> {
 export async function encrypt (data: string, address: string): Promise<string> {
   const publicKey = await getEncryptionKey(address)
   const ev = await x25519xsalsa20poly1305.asymmetricEncrypt(decodeFromString(publicKey, 'base64'), decodeFromString(data, 'utf8'))
-  return x25519xsalsa20poly1305.stringifyEncrypedData(ev, 'hex')
+  return stringifyEncryptedData(ev, 'hex')
 }
 
 export async function decrypt (cipherText: string, account: string): Promise<string> {
