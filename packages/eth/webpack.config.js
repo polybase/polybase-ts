687ea3053eb44216ed02d95c5ae167fb389cc4a2
Cal <calum.moore@gmail.com>
2022-12-22 01:01:35 +0000

    Export minified umd format for cdn (#54)

---
+++ packages/eth/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  mode: 'production', // build for production (minifies the output)
+  entry: './src/index.ts', // entry point for your library
+  output: {
+    path: path.resolve(__dirname, 'dist'), // output directory
+    filename: 'bundle.min.js', // output file name (minified)
+    library: 'polybase_eth', // name of the global variable that will contain your library
+    libraryTarget: 'umd', // format of the bundle (UMD supports both CommonJS and AMD module systems)
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'], // resolve TypeScript and JavaScript files
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // apply to TypeScript files
+        use: 'ts-loader', // use the ts-loader to transpile the TypeScript code
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false, // remove comments from the output
+          },
+        },
+      }),
+    ],
+  },
+}
---
+++ packages/util/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  mode: 'production', // build for production (minifies the output)
+  entry: './src/index.ts', // entry point for your library
+  output: {
+    path: path.resolve(__dirname, 'dist'), // output directory
+    filename: 'bundle.min.js', // output file name (minified)
+    library: 'polybase_util', // name of the global variable that will contain your library
+    libraryTarget: 'umd', // format of the bundle (UMD supports both CommonJS and AMD module systems)
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'], // resolve TypeScript and JavaScript files
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      process: require.resolve('process/browser'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // apply to TypeScript files
+        use: 'ts-loader', // use the ts-loader to transpile the TypeScript code
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false, // remove comments from the output
+          },
+        },
+      }),
+    ],
+  },
+}
